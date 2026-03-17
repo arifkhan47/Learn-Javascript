06e0f49ae513a86662ea1574ce2ea3f40e34c35d
arifkhan47 <51923405+arifkhan47@users.noreply.github.com>
2026-03-17 08:36:06 +0000

    Variables file

---
+++ .devcontainer/01_Basic/variables.js
@@ -0,0 +1,11 @@
+const userId = 000049809343;
+let userName = "Arif khan";
+var userEmail = "arifkhanafrn@gmail.com"; /* Please avoid this var because of this you will face issue in block scope and funtional scope */
+
+userState = "Pune" /*It will be work but its wrong practice dont write like this*/
+
+
+console.log(userId);
+
+
+console.table([userId, userName, userEmail, userState])
