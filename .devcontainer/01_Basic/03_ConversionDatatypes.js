0393668ba5df11c3eb99fcb07fe12cc83cd22021
arifkhan47 <51923405+arifkhan47@users.noreply.github.com>
2026-03-17 12:09:56 +0000

    Conversion datatypes

---
+++ .devcontainer/01_Basic/03_ConversionDatatypes.js
@@ -0,0 +1,31 @@
+let score = "45fs";
+let numberScore = Number(score);
+console.log(typeof (numberScore));
+
+
+console.log(numberScore);
+
+/*
+45 => 45 number
+"45" => 45 number
+"45sfd" => NaN
+
+*/
+
+
+let matchNo = null
+
+let changeMatchNo = Number(matchNo);
+console.log(changeMatchNo);
+
+
+let booleanValue = "arif";
+let changeBoolean = Boolean(booleanValue);
+console.log(changeBoolean);
+
+
+/* 
+true => 1     false => 0 
+"" => false
+"arif" => true
+*/
