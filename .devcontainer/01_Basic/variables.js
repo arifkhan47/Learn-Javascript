c50d8fd081d73a4b24eb60976d2e504f64aa786c
arifkhan47 <51923405+arifkhan47@users.noreply.github.com>
2026-03-17 08:41:24 +0000

    undifined declaired

--- .devcontainer/01_Basic/variables.js
+++ .devcontainer/01_Basic/variables.js
@@ -2,10 +2,11 @@ const userId = 49809343;
 let userName = "Arif khan";
 var userEmail = "arifkhanafrn@gmail.com"; /* Please avoid this var because of this you will face issue in block scope and funtional scope */
 
-userState = "Pune" /*It will be work but its wrong practice dont write like this*/
+userCity = "Pune"; /*It will be work but its wrong practice dont write like this*/
+let userState;
 
 
 console.log(userId);
 
 
-console.table([userId, userName, userEmail, userState])
+console.table([userId, userName, userEmail, userCity, userState])
