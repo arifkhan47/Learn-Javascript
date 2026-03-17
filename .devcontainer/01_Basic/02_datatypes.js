ac7c752d4d4e1bfb0748f2d0b22a4b835d249e6c
arifkhan47 <51923405+arifkhan47@users.noreply.github.com>
2026-03-17 09:19:59 +0000

    datatypes

---
+++ .devcontainer/01_Basic/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"  // Treat all JS code an newer stander JS code
+
+// alert(4 + 9); //we are using node, not browser
+
+
+console.log( 9 
+    +
+    5);  // Code readability should be higher
+    
+console.log("Arif");
+
+// nn bb ss u   // there are 7 datatypes in javascript
+
+// number =  4585
+// null 
+// boolean = yes/no
+// bgInt =   this types use for big numbers
+// string = "arif"
+// symbol = @
+// undifined = i means variavle declaired but but value is not assigned
+
+
+// Non premative datatypes
+// object 
+
+console.log(typeof "arif");   // string
+console.log(typeof null);     // object
+console.log(typeof undefined);   // undefined
+
+
