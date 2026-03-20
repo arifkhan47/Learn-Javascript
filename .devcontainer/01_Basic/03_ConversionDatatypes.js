08fc0eed0c45e7af6504639f3c70219021628e86
arifkhan47 <51923405+arifkhan47@users.noreply.github.com>
2026-03-20 09:01:14 +0000

    comparision

--- .devcontainer/01_Basic/03_ConversionDatatypes.js
+++ .devcontainer/01_Basic/03_ConversionDatatypes.js
@@ -29,3 +29,70 @@ true => 1     false => 0
 "" => false
 "arif" => true
 */
+
+
+
+
+// Datatypes operations
+
+// "1" + 5 = 15
+// "1" + "5" = 15
+// "1" + 5 + 6 = 156
+// 1 + 5 + "6" = 66
+
+// let a = "25"
+// let b = Number(a) + 10;
+// console.log(b);
+
+// console.log(typeof null)   // this output will be come object
+
+// console.log("10" + 5 * 2)
+
+// let value = "0"
+
+// if(value) {
+//   console.log("True hai")
+// } else {
+//   console.log("False hai")
+// }
+
+
+// false, 0, "", null, undefined, NaN    // this all values are false
+
+// console.log(Boolean("0"))
+// console.log(Boolean(" "))
+
+// console.log(Number("50abc"))
+// console.log(parseInt("50abc"))
+
+// console.log("5" + 5)
+// console.log("5" - 5)
+
+// let a = "20"
+// let b = "10"
+
+// console.log(a + b)
+// console.log(Number(a) + Number(b))
+
+// console.log(Boolean(null))
+// console.log(Boolean(undefined))
+// console.log(Boolean(NaN))
+
+// console.log([] + [])   /  [] js convert this into "" sstring thats why "" + "" = blank space
+
+// console.log(" " == 0)
+// console.log(" " === 0)
+
+// let x = "100px"
+// let y = Number(x)
+
+// console.log(y)
+
+console.log(false == "")
+console.log(false === "")
+
+console.log("1" == true)
+console.log("1" === true)
+
+console.log([] == false)
+console.log([] === false)
