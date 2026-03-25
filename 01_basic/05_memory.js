386d2ae646132f240457793cf1add07de037da6a
arifkhan47 <51923405+arifkhan47@users.noreply.github.com>
2026-03-25 12:18:45 +0000

    memory file

--- 01_basic/05_memory.js
+++ 01_basic/05_memory.js
@@ -4,13 +4,14 @@
 
 // 1. stack Memory (Primitive datatypes)
 
-let name = "Arif";
+// let name = "Arif";
 
-let realName = name;
 
-realName = "ayaz"
+// let realName = name;
 
-console.log(realName); // ayaz (he only copy the value)
+// realName = "ayaz"
+
+// console.log(realName); // ayaz (he only copy the value)
 
 
 
@@ -24,6 +25,44 @@ let user = {
 
 let costumer = user;
 costumer.userName = "aslam";
-console.log(user.userName);
+// console.log(user.userName);
 
+
+// let name = "   arif khan   "
+
+let name = "Arif"
+
+let nameString = name.toUpperCase();
+
+console.log(nameString.slice(0, 1));
+
+
+
+// console.log(name.at(3));
+// console.log(name.at(-1));
+
+
+
+// console.log(name.trim());
+
+let text = "hello";
+console.log(text.replace("hello", "olleh"));
+
+
+
+
+
+// console.log(text.includes("rifk"));
+// console.log(text.includes("python"));
+
+let text1 = "apple,banana,mango"
+
+// console.log(text1.split(","));
+
+
+let email = "   arif@gmail.com   "
+
+
+// console.log(email.trim());
+// console.log(email.includes("@gmail.com"));
 
